b614f983b0773c4257b2fa9070f7555a54f8269c
neacsumihai2005 <neacsumihai525@gmail.com>
2024-05-04 20:33:04 +0300

    Massive rework

---
+++ fitness.h
@@ -0,0 +1,275 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+namespace workoutDiary{
+
+    class exerciseType{
+        private:
+            std::string name;
+            std::string description;
+            std::map<int, int> recordReps;
+            ///recordReps[weight] = PR of repetitions for the weight
+
+        public:
+            exerciseType(){}
+            exerciseType(std::string newName){
+                name = newName;
+            }
+            exerciseType(std::string newName, std::string newDescription){
+                name = newName;
+                description = newDescription;
+            }
+            ~exerciseType() = default;
+
+            std::string getName(){
+                return name;
+            }
+            void setName(std::string newName){
+                name = newName;
+            }
+
+            std::string getDescription(){
+                return description;
+            }
+            void setDescription(std::string newDescription){
+                description = newDescription;
+            }
+
+
+            forceUpdatePR(int weight, int reps){
+                if(recordReps.find(weight) == recordReps.end()){
+                    recordReps[weight] = reps;
+                }
+                else {
+                    recordReps[weight] = reps;
+                }
+            }
+            tryUpdatePR(int weight, int reps){
+                if(recordReps.find(weight) == recordReps.end()){
+                    recordReps[weight] = reps;
+                }
+                else {
+                    recordReps[weight] = std::max(
+                                             recordReps[weight],
+                                             reps
+                                             );
+                }
+            }
+            getPR(int weight){
+                if(recordReps.find(weight) == recordReps.end()){
+                    return 0;
+                }
+                return recordReps[weight];
+            }
+
+            void printRecords(std::ostream & out, bool userFriendly){
+                if(userFriendly){
+                    out << "- Records for the [" << name << "] exercise:" << "\n";
+                    for(auto it : recordReps){
+                        ///out << it.first << "kilograms: " << it.second << "\n";
+                        out << "** " << it.second << " reps with : " << it.first << " kilograms" << "\n";
+                    }
+                    out << "\n";
+                }
+                else { ///developer friendly
+                    out << recordReps.size() << "\n";
+                    for(auto it : recordReps){
+                        out << it.first << ' ' << it.second << "\n";
+                    }
+                }
+            }
+    };
+
+    class exercise{
+        private:
+            exerciseType * currentExerciseType;
+            int weight;
+            std::vector<int> listReps;
+
+        public:
+            exercise(){}
+
+            exercise(exerciseType * newCurrentExerciseType, int newWeight, std::vector<int> newListReps){
+                currentExerciseType = newCurrentExerciseType;
+                weight = newWeight;
+                listReps = newListReps;
+            }
+
+            ~exercise() = default;
+
+
+            exerciseType * getCurrentExerciseType(){
+                return currentExerciseType;
+            }
+            void setCurrentExercise(exerciseType * newCurrentExerciseType){
+                currentExerciseType = newCurrentExerciseType;
+            }
+
+            int getWeight(){
+                return weight;
+            }
+            void setWeight(int newWeight){
+                weight = newWeight;
+            }
+
+            std::vector<int> getListReps(){
+                return listReps;
+            }
+            void setListReps(std::vector<int> newListReps){
+                listReps = newListReps;
+            }
+
+            void printReps(std::ostream & out){
+                for(int i = 0; i < listReps.size(); i++){
+                    out << listReps[i] << ' ';
+                }
+            }
+
+            void addSet(int reps){
+                listReps.push_back(reps);
+            }
+
+    };
+
+    class calendaristicDate{
+        private:
+            int day;
+            int month;
+            int year;
+
+        public:
+            calendaristicDate(){}
+            calendaristicDate(int newDay, int newMonth, int newYear){
+                day = newDay;
+                month = newMonth;
+                year = newYear;
+            }
+            ~calendaristicDate() = default;
+
+            int getDay(){
+                return day;
+            }
+            void setDay(int newDay){
+                day = newDay;
+            }
+
+            int getMonth(){
+                return month;
+            }
+            void getMonth(int newMonth){
+                month = newMonth;
+            }
+
+            int getYear(){
+                return year;
+            }
+            void setYear(int newYear){
+                year = newYear;
+            }
+
+            operator = (calendaristicDate const &operand){
+                day = operand.day;
+                month = operand.month;
+                year = operand.year;
+            }
+            friend std::ostream& operator << (std::ostream& out, calendaristicDate const& X){
+                if(X.day <= 9){
+                    out << "0";
+                } out << X.day;
+                out << "/";
+                if(X.month <= 9){
+                    out << "0";
+                } out << X.month;
+                out << "/";
+                out << X.year;
+            }
+            bool leapYear(){
+                if (year % 400 == 0) {
+                    return true;
+                }
+                else if(year % 100 == 0){
+                    return false;
+                }
+                else if(year % 4 == 0){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            bool validDate(){
+                if(day < 0 || day > 31){
+                    return false;
+                }
+                if(month < 0 || month > 12){
+                    return false;
+                }
+                switch (month){
+                    case 1: ///ianuarie
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 2: ///februarie
+                        if(leapYear()){
+                            return (1 <= day && day <= 29);
+                        }
+                        else {
+                            return (1 <= day && day <= 28);
+                        }
+                        break;
+                    case 3: ///martie
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 4: ///aprilie
+                        return (1 <= day && day <= 30);
+                        break;
+                    case 5: ///mai
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 6: ///iunie
+                        return (1 <= day && day <= 30);
+                        break;
+                    case 7: ///iulie
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 8: ///august
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 9: ///septembrie
+                        return (1 <= day && day <= 30);
+                        break;
+                    case 10: ///octombrie
+                        return (1 <= day && day <= 31);
+                        break;
+                    case 11: ///noiembrie
+                        return (1 <= day && day <= 30);
+                        break;
+                    case 12: ///decembrie
+                        return (1 <= day && day <= 31);
+                        break;
+                }
+}
+    };
+
+    class workout{
+        private:
+            calendaristicDate date;
+            std::vector<exercise*> listExercises;
+
+        public:
+            workout(calendaristicDate newDate){
+                date = newDate;
+            }
+            workout(calendaristicDate newDate, std::vector<exercise*> newListExercises){
+                date = newDate;
+                listExercises = newListExercises;
+            }
+            ~workout() = default;
+
+
+            void insertExercise(exercise* newExercise){
+                listExercises.push_back(newExercise);
+            }
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,327 +1,251 @@
-/*
-    Am 3 clase:
-    Antrenament: tine:
-        -data efectuarii
-        -o lista de Bubbles
-
-    Bubble: tine un Exercitiu impreuna cu greutatea efectuata si o lista de seturi, exemplu:
-    flotari: 80kg
-    10 20 30
-    inseamna ca s-au facut 3 seturi de 10, 20, 30 repetitii cu 80kg
-    variaza kilogramele la fiecare exercitiu pt ca se pot adauga veste cu greutate care sa adauge kg; noi notam aici
-    kilogramele totale
-
-    Exercitiu: tine numele unui exercitiu si recordurile pentru fiecare greutate efectuata, printr-un map
-    --in editiile urmatoare va tine si descrierea fiecarui exercitiu
-
-    deci Exercitiul e ceva general, pe cand un Bubble e ceva mai local, efectuata in fiecare antrenament
-
-    planuri pt editiile urmatoare:
-    sa aranjez mai bine clasele
-    sa adaug functii noi la clasa de Exercitiu si la clasa de Antrenament (de exemplu, observatii pt cum m-am simtit
-                                                                           in timpul acelui antrenament sau cat de grele
-                                                                           s-au simtit exercitiile)
-    sa adaug o clasa noua de Jurnal, care sa fie practic baza tuturora si sa le lege impreuna mai bine
-
-*/
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <map>
-#include <assert.h>
 
-using namespace std;
+#include "fitness.h"
 
+using namespace workoutDiary;
 
-class Exercitiu;
-class Bubble;
-class Antrenament;
-
-
-struct data_calendar{
-    int zi;
-    int luna;
-    int an;
-
-    friend ostream& operator << (ostream& out, data_calendar const& X){
-        out << X.zi << ' ' << X.luna << ' ' << X.an << "\n";
-    }
-};
-
-/*
-struct Set_Exercitiu{
-    int greutate;
-    int repetitii;
-};
-*/
-class Exercitiu{
-    private:
-        string nume;
-        string descriere;
-        map<int, int> record_reps; ///record_reps[greutate] = maximul de repetari cu greutatea aia
-
-
-    public:
-        Exercitiu(){
-        }
-        Exercitiu(string ex){
-            nume = ex;
-        }
-        Exercitiu(string ex, string desc){
-            nume = ex;
-            descriere = desc;
-        }
-
-        tryToUpdatePR(int greutate, int nr_repetari){
-            if(record_reps.find(greutate) == record_reps.end()){
-                ///e prima oara cand fac cu greutatea asta deci devine record pt greutatea respectiva
-                record_reps[greutate] = nr_repetari;
-            }
-            else {
-                record_reps[greutate] = max(record_reps[greutate],
-                                            nr_repetari
-                                            );
-            }
-        }
-
-        string getNume(){
-            return nume;
-        }
-
-        void printRecords(){
-            cout << "Recorduri la " << nume << ":\n";
-            for(auto it : record_reps){
-                cout << it.second << " repetitii cu " << it.first << " kg\n";
-            }
-            cout << "\n";
-        }
-
-        friend class Bubble;
-};
-
-
-class Bubble{
-    private:
-        ///Exercitiu exercitiul_curent;
-        string nume_exercitiu;
-        int greutate;
-        vector<int> lista_repetitii;
-
-    public:
-        ~Bubble(){
-            nume_exercitiu.erase();
-            lista_repetitii.clear();
-        }
-        Bubble (const Bubble& t){
-            nume_exercitiu = t.nume_exercitiu;
-            greutate = t.greutate;
-
-            lista_repetitii.clear();
-            for(int i = 0; i < t.lista_repetitii.size(); i++){
-                lista_repetitii.push_back(t.lista_repetitii[i]);
-            }
-        }
-
-        Bubble (string ex, auto &listaExercitii){
-            nume_exercitiu = ex;
-            /*
-                daca ex nu se afla in lista de exercitii ii aloc spatiul
-            */
-            if( listaExercitii.find(ex) == listaExercitii.end() ){
-                ///am un exercitiu ne-mai-intalnit
-                listaExercitii.insert(make_pair(ex, Exercitiu(ex)));
-            }
-            /*else {
-
-            }*/
-        }
-
-        void setGreutate(int X){
-            greutate = X;
-        }
-        string getExercitiu(){
-            return nume_exercitiu;
-        }
-        void afis_reps(){
-            for(int i = 0; i < lista_repetitii.size(); i++){
-                cout << lista_repetitii[i] << ' ';
-            }
-        }
-        void adaugaSet(string ex, int greutate, int reps, auto &listaExercitii){
-            lista_repetitii.push_back(reps);
-
-            listaExercitii[ex].tryToUpdatePR(greutate, reps);
-        }
-
-        friend ostream& operator << (ostream& out, Bubble &X){
-            cout << X.nume_exercitiu << " : ";
-            for(int i = 0; i < X.lista_repetitii.size(); i++){
-                cout << X.lista_repetitii[i] << ' ';
-            }
-            cout << "\n";
-        }
-
-
-        operator=(Bubble &t){
-            nume_exercitiu = t.nume_exercitiu;
-            greutate = t.greutate;
-
-            lista_repetitii.clear();
-            for(int i = 0; i < t.lista_repetitii.size(); i++){
-                lista_repetitii.push_back(t.lista_repetitii[i]);
-            }
-        }
-
-
-
-        friend class Jurnal;
-        friend class Exercitiu;
-
-
-};
-
-class Antrenament{
-    private:
-        data_calendar when;
-        vector<Bubble> lista_Bubbles;
-    public:
-        Antrenament(int z, int l, int a){
-            when.zi = z;
-            when.luna = l;
-            when.an = a;
-        }
-
-        void adaugaBubble(Bubble X){
-            lista_Bubbles.push_back(X);
-        }
-
-        friend ostream& operator << (ostream& out, Antrenament & X){
-            out << "Antrenamentul din " << X.when << ":\n";
-            for(int i = 0; i < X.lista_Bubbles.size(); i++){
-                cout << X.lista_Bubbles[i].getExercitiu() << " : ";
-                X.lista_Bubbles[i].afis_reps();
-                cout << "\n";
-            }
-            cout << "\n";
-        }
-};
-
-
-/*
-class Set_Exercitiu{
-    private:
-        string nume;
-        int greutate;
-        int repetitii;
-
-    public:
-
-};*/
-
-
-inline void afisare_optiuni_0(){
-    cout << "1 = inceput antrenament nou" << "\n";
-    cout << "2 = afisare recorduri" << "\n";
+inline void printMainScreenOptions(std::ostream & out){
+    out << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << "\n";
+    out << "~ 1 = start a new workout           ~" << "\n";
+    out << "~ 2 = modify records                ~" << "\n";
+    out << "~ 3 = print current global PRs      ~" << "\n";
+    out << "~ other number = exit               ~" << "\n";
+    out << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << "\n";
 }
-inline void afisare_optiuni_2(){
-    cout << "1 = afisarea tuturor recordurilor" << "\n";
-    cout << "2 = afisarea unui record specific" << "\n";
+inline void printModifyScreenOptions(std::ostream & out){
+    out << "------------------------------------" << "\n";
+    out << "- 1 = delete a certain exercise    -" << "\n";
+    out << "- 2 = modify a certain PR manually -" << "\n";
+    out << "- other number = exit              -" << "\n";
+    out << "------------------------------------" << "\n";
+}
+void printAllExercises(std::ostream & out, std::map<std::string, exerciseType*> const &exerciseMap){
+    int iteratorNumber = 1;
+    for(auto it : exerciseMap){
+        out << iteratorNumber << ". " << it.first << "\n";
+        iteratorNumber++;
+    }
 }
 
-void start_antrenament(auto &listaExercitii){
+inline void menuNewWorkout(std::map<std::string, exerciseType*> &exerciseMap){
+    std::cout << "Day = "; int day; std::cin >> day;
+    std::cout << "Month = "; int month; std::cin >> month;
+    std::cout << "Year = "; int year; std::cin >> year;
 
-    cout << "Ziua = "; int ziua; cin >> ziua;
-    cout << "Luna = "; int luna; cin >> luna;
-    cout << "Anul = "; int anul; cin >> anul;
-    Antrenament antrenament_nou(ziua, luna, anul);
-
-    cout << "Denumire exercitiu curent = ";
-    string denumire_exercitiu;
-    cin >> denumire_exercitiu;
-
-    while(denumire_exercitiu != "stop"){
-        Bubble bubble_nou(denumire_exercitiu, listaExercitii);
-
-        cout << "Greutatea (kg) = ";
-        int greutate;
-        cin >> greutate;
-        bubble_nou.setGreutate(greutate);
-
-        cout << "Set de ";
-        int nr_repetari;
-        cin >> nr_repetari;
-
-        int nr_seturi = 0;
-        while(nr_repetari != 0){
-            bubble_nou.adaugaSet(denumire_exercitiu, greutate, nr_repetari, listaExercitii);
-
-            cout << "Set de ";
-            cin >> nr_repetari;
-        }
-
-        cout << bubble_nou << "\n";
-
-        antrenament_nou.adaugaBubble(bubble_nou);
-
-
-        cout << "Denumire exercitiu curent (sau scrie stop pt a opri) = ";
-        cin >> denumire_exercitiu;
+    if(!(calendaristicDate(day, month, year).validDate())){
+        throw(std::runtime_error("Invalid date!"));
     }
 
-    ///listaAntrenamente.push_back(antrenament_nou);
+    std::cout << "!!Type \"stop\" in the Exercise box in order to end the workout!!" << "\n";
+    workout* temp = new workout(calendaristicDate(day, month, year));
+
+    std::cout << "Exercise = ";
+    std::string currentExerciseName;
+    std::cin >> currentExerciseName;
+
+    while(currentExerciseName != "stop"){
+        exercise * currentExercise = new exercise();
+        exerciseType * currentExerciseType;
+
+        ///this might be a new exercise, so we verify that
+        if(exerciseMap.find(currentExerciseName) == exerciseMap.end()){
+            currentExerciseType = new exerciseType(currentExerciseName);
+            exerciseMap[currentExerciseName] = currentExerciseType;
+
+            std::cout << "New exercise detected!" << "\n";
+            std::cout << "Would you like to add a description? (y/n)" << "\n";
+
+            char userChoice;
+            std::cin >> userChoice;
+            if( !(userChoice == 'n' || userChoice == 'y') ){
+                throw(std::runtime_error("Invalid choice!"));
+            }
+            if(userChoice == 'y'){
+                std::cout << "Description (one liner) = ";
+                std::string newDescription;
+                std:: cin >> newDescription;
+                currentExerciseType -> setDescription(newDescription);
+            }
+
+        }
+        else {
+            currentExerciseType = exerciseMap[currentExerciseName];
+        }
+
+        std::cout << "Weight (kg) = ";
+        int weight;
+        std::cin >> weight; currentExercise -> setWeight(weight);
+
+        std::cout << "Reps = ";
+        int reps;
+        std::cin >> reps;
+        while(reps != 0){
+            currentExercise -> addSet(reps);
+
+            int previousPR = currentExerciseType -> getPR(weight);
+            currentExerciseType -> tryUpdatePR(weight, reps);
+            int potentialPR = currentExerciseType -> getPR(weight);
+
+            if(potentialPR > previousPR){
+                std::cout << "Congratulations! You just hit a PR!\n";
+            }
+
+            std::cin >> reps;
+        }
+
+        std::cout << "Exercise = ";
+        std::cin >> currentExerciseName;
+    }
+
+    std::cout << "Workout ended! Congratulations!" << "\n" << "\n";
+}
+
+void getExerciseMap(std::istream &in, std::map<std::string, exerciseType*> &exerciseMap){
+    std::string currentExerciseName;
+    while(in >> currentExerciseName){
+        exerciseType * newEntry = new exerciseType(currentExerciseName);
+
+        int numberOfRecords;
+        in >> numberOfRecords;
+        for(int i = 0; i < numberOfRecords; i++){
+            int weight;
+            in >> weight;
+
+            int reps;
+            in >> reps;
+
+            newEntry -> tryUpdatePR(weight, reps);
+        }
+
+
+        ///we built newEntry; now we add it to the exerciseMap
+        exerciseMap[currentExerciseName] = newEntry;
+    }
+}
+
+void outputExerciseMap(std::ostream& out, std::map<std::string, exerciseType*> const & exerciseMap){
+    for (auto it : exerciseMap){
+        out << (it.second) -> getName() << "\n";
+        (it.second) -> printRecords(out, false);
+    }
 }
 
 int main()
 {
-    ifstream fin ("test.in");
-    ofstream fout ("test.out");
+    std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << "\n";
+    std::cout << "!!Disclaimer: please use the singular form for each exercise         !!" << "\n";
+    std::cout << "!!For example: \"dip\" and \"dips\" are registered as different exercises!!" << "\n";
+    std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << "\n";
+    std::cout << "\n";
 
-    map<string, Exercitiu> listaExercitii; ///lista exercitiilor existente
+    int userCurrentChoice = 0;
+    ///cute little menu implementation
+    std::ifstream fin ("test.txt");
+    ///updateInput(fin);
 
-    afisare_optiuni_0();
-    int optiune;
-    cin >> optiune;
-    assert(1 <= optiune && optiune <= 2);
+    std::map<std::string, exerciseType*> exerciseMap;
+    getExerciseMap(fin, exerciseMap);
 
-    while(1 <= optiune && optiune <= 2){
-
-        if(optiune == 1){ ///antrenament nou
-            start_antrenament(listaExercitii);
-        }
-        else if(optiune == 2){ ///afisare recorduri
-
-            afisare_optiuni_2();
-            cin >> optiune;
-            assert(optiune == 1 || optiune == 2);
-
-            if(optiune == 1){ ///afisez toate recordurile
-                for(auto it : listaExercitii){
-                    it.second.printRecords();
-                }
-            }
-            else { ///introduc exercitiul pt care vreau sa vad recordul
-                cout << "Afiseaza recordurile pt exercitiul: ";
-                string exercitiu;
-                cin >> exercitiu;
-
-                if(listaExercitii.find(exercitiu) != listaExercitii.end()){
-                    listaExercitii[exercitiu].printRecords();
-                }
-                else {
-                    cout << "Nu am facut inca exercitiul asta\n";
-                }
-                /*{for x in lista_exercitii:
-                    cout << i << ' ' << x << "\n";
-
-                cin >> numar
-                cout << record[exercitiu[numar]] << "\n";
-
-                }*/
-            }
-        }
-
-        afisare_optiuni_0();
-        cin >> optiune;
+    ///std::cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" << "\n";
+    std::cout << "Here are your current lifetime records:" << "\n";
+    for (auto it : exerciseMap){
+        ///std::cout << it.first << "!";
+        it.second -> printRecords(std::cout, true);
+        ///std::cout << "\n";
     }
+    ///std::cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" << "\n";
+    std::cout << "\n";
+
+    printMainScreenOptions(std::cout);
+    std::cin >> userCurrentChoice;
+
+    while(userCurrentChoice == 1 || userCurrentChoice == 2 || userCurrentChoice == 3){
+        if(userCurrentChoice == 1){
+            ///start a new workout
+            menuNewWorkout(exerciseMap);
+        }
+        else if(userCurrentChoice == 2){
+            ///modify records
+            printModifyScreenOptions(std::cout);
+            std::cin >> userCurrentChoice;
+
+            if(userCurrentChoice == 1){
+                ///delete an exercise altogether
+                printAllExercises(std::cout, exerciseMap);
+                std::cout << "Which exercise do you want to delete? (" << 1 << " to " << exerciseMap.size() << ")" << "\n";
+
+                int indexToDelete;
+                std::cout << "Number = ";
+                std::cin >> indexToDelete;
+                if( ! (1 <= indexToDelete && indexToDelete <= exerciseMap.size()) ){
+                    throw(std::runtime_error("Invalid number!"));
+                }
+
+
+                auto iteratorToDelete = exerciseMap.begin();
+                for(int times = 1; times <= indexToDelete - 1; times++){
+                    iteratorToDelete++;
+                }
+                delete iteratorToDelete -> second; ///free up memory
+                exerciseMap.erase(iteratorToDelete);
+
+                printAllExercises(std::cout, exerciseMap);
+
+            }
+            else if(userCurrentChoice == 2){
+                ///modify a certain PR manually
+                printAllExercises(std::cout, exerciseMap);
+                std::cout << "Which exercise do you want to update? (" << 1 << " to " << exerciseMap.size() << ")" << "\n";
+
+                int indexToUpdate;
+                std::cout << "Number = ";
+                std::cin >> indexToUpdate;
+                if( ! (1 <= indexToUpdate && indexToUpdate <= exerciseMap.size()) ){
+                    throw(std::runtime_error("Invalid number!"));
+                }
+
+
+                auto iteratorToUpdate = exerciseMap.begin();
+                for(int times = 1; times <= indexToUpdate - 1; times++){
+                    iteratorToUpdate++;
+                }
+
+                std::cout << "Weight to update (kg) = ";
+                int weightToUpdate;
+                std::cin >> weightToUpdate;
+                if( ! (weightToUpdate > 0) ){
+                    throw(std::runtime_error("Negative weight!"));
+                }
+
+                std::cout << "New PR for reps = ";
+                int repsToUpdate;
+                std::cin >> repsToUpdate;
+                if( ! (repsToUpdate > 0) ){
+                    throw(std::runtime_error("Negative reps!"));
+                }
+
+                (iteratorToUpdate -> second) -> forceUpdatePR(weightToUpdate, repsToUpdate);
+            }
+        }
+        else if(userCurrentChoice == 3){
+            ///show current global PRs
+            for (auto it : exerciseMap){
+                ///std::cout << it.first << "!";
+                it.second -> printRecords(std::cout, true);
+                ///std::cout << "\n";
+            }
+        }
+
+        printMainScreenOptions(std::cout);
+        std::cin >> userCurrentChoice;
+    }
+
+    fin.close();
+    printAllExercises(std::cout, exerciseMap);
+
+    std::ofstream fout ("test.txt");
+    outputExerciseMap(fout, exerciseMap);
+
     return 0;
 }
